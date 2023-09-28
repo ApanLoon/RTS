460a81535341ca00c6f0f044a3891e64b8873ff5
ApanLoon <apanloon@live.co.uk>
2023-09-28 17:27:17 +0200

    Use mouse delta instead of mouse position for camera orbit
    Solves the "stop at screen border" issue.

--- src/RTS/Assets/Scripts/Controllers/CameraRigController.cs
+++ src/RTS/Assets/Scripts/Controllers/CameraRigController.cs
@@ -9,18 +9,16 @@ public class CameraRigController : MonoBehaviour
     /// <summary>
     /// Min and max of the camera orbit pitch in degrees
     /// </summary>
-    public Vector2 OrbitPitchLimits = new Vector2(-10, 80);
+    public Vector2 OrbitPitchLimits = new(-10, 80);
 
     public float DollySensitivity = 1.0f;
     /// <summary>
     /// Min and max of the camera dolly in meters
     /// </summary>
-    public Vector2 DollyLimits = new Vector2(2f, 25f);
+    public Vector2 DollyLimits = new(2f, 25f);
 
 
     private Camera _camera;
-
-    private Vector2? _prevOrbitInput = null;
 
     private void Start()
     {
@@ -41,34 +39,26 @@ public class CameraRigController : MonoBehaviour
         var dir = transform.right * input.x;
         dir += new Vector3(transform.forward.x, 0f, transform.forward.z) * input.y;
         dir.Normalize();
-        transform.Translate (dir * PanSpeed * deltaTime, Space.World);
+        transform.Translate (dir * (PanSpeed * deltaTime), Space.World);
 
         ClampToTerrain();
         ManageCollisions();
     }
 
     /// <summary>
-    /// Handle camera orbit input
+    /// Handle camera orbit inputDelta
     /// </summary>
-    /// <param name="input">Mouse position in screen coordinates</param>
+    /// <param name="isOrbiting">True if camera orbit is active this frame. On the frame when orbiting stops, this will be false.</param>
+    /// <param name="inputDelta">Mouse position in screen coordinates</param>
     /// <param name="deltaTime">Time since last frame</param>
-    private void OnCameraOrbit(bool isOrbiting, Vector2 input, float deltaTime)
+    private void OnCameraOrbit(bool isOrbiting, Vector2 inputDelta, float deltaTime)
     {
-        // TODO: How do we handle infinite orbiting when the mouse reaches the edges of the screen?
-
         if (isOrbiting == false)
         {
-            _prevOrbitInput = null;
             return;
         }
 
-        if (_prevOrbitInput == null)
-        {
-            _prevOrbitInput = input;
-            return;
-        }
-
-        Vector2 delta = (input - (Vector2)_prevOrbitInput) * OrbitSensitivity * deltaTime;
+        var delta = inputDelta * (OrbitSensitivity * deltaTime);
         if (InvertPitch)
         {
             delta.y *= -1;
@@ -78,12 +68,10 @@ public class CameraRigController : MonoBehaviour
         transform.Rotate(new Vector3(0f,      delta.x, 0f), Space.World);
 
         // Clamp x-rotation
-        Vector3 rotEuler = transform.rotation.eulerAngles;
+        var rotEuler = transform.rotation.eulerAngles;
         transform.rotation = Quaternion.Euler(new Vector3(rotEuler.x.ClampAngle(OrbitPitchLimits.x, OrbitPitchLimits.y), rotEuler.y, rotEuler.z));
 
         _camera.transform.LookAt(transform, Vector3.up);
-
-        _prevOrbitInput = input;
 
         ClampToTerrain();
         ManageCollisions();
@@ -96,8 +84,8 @@ public class CameraRigController : MonoBehaviour
         _camera.transform.Translate(0f, 0f, input * DollySensitivity, Space.Self);
 
         // Clamp local z-position
-        Vector3 pos = _camera.transform.localPosition;
-        Vector3 clamped = new Vector3 (pos.x, pos.y, Mathf.Clamp(pos.z, -DollyLimits.y, -DollyLimits.x));
+        var pos = _camera.transform.localPosition;
+        var clamped = new Vector3 (pos.x, pos.y, Mathf.Clamp(pos.z, -DollyLimits.y, -DollyLimits.x));
         _camera.transform.localPosition = clamped;
 
         ClampToTerrain();
